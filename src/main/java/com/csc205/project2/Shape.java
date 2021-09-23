cf925d80215dd280d80b2249c5e1303d82bd044a
ZakeryKeahey <zak2151373@maricopa.edu>
2021-09-23 13:09:51 -0700

    Project-2

---
+++ src/main/java/com/csc205/project2/Shape.java
@@ -0,0 +1,16 @@
+package com.csc205.project2;
+
+public class Shape implements ThreeDimensionalShape{
+    public Shape() {
+    }
+
+    @Override
+    public double surfaceArea() {
+        return 0;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+}
